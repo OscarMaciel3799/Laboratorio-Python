0d3a57893d86069919bf3d394eb873840ecad3e8
Oscar <oscarfranciscomaciel@gmail.com>
2025-07-20 17:42:06 -0300

    Creacion del archivo principal

---
+++ App/EcoTracker.py
@@ -0,0 +1,9 @@
+
+"""
+EcoTracker - Monitor de Consumo Energético Digital
+Trabajo Final - Algoritmos y Estructuras de Datos
+Principios de Green Software aplicados
+
+Autores: [Nombres del grupo]
+Fecha: Julio 2025
+"""
